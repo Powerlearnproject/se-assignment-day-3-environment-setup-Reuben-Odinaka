900f380cab8ed671121d5f09cee47558743acf3a
Reuben-Odinaka <reubenodinakachukwu@gmail.com>
2024-08-24 14:07:29 +0100

    first commit

---
+++ Navigation.py
@@ -0,0 +1,34 @@
+#Variables
+#let's store a favorite number
+# Favorite_Number = 73
+# print(f'My Favorite Number is: {Favorite_Number}')
+
+#String
+# greeting = "Hello, PLP Academy"
+# print(greeting)
+
+# #Boolean
+# is_python_fun = True
+# is_python_boring = False
+# print(f'is Python fun? {is_python_fun}')
+# print(f'Is Python Boring? {is_python_boring}')
+
+#Control_Flow
+# temperature = 30
+# if temperature > 25:
+#     print("It's hot outside! War shorts.")
+# elif temperature > 15:
+#     print("it's warm. Maybe wear a light Jacket")
+# else:
+#     print("Brrrr! It's cold. Bundle Up!")
+
+#Module
+#In-Built Module
+# import math
+# result = math.sqrt(144)
+# print(f'The square root of 144 is {result}')
+
+#Customized Module
+# import greetings
+# print(greetings.greet("Mark"))
+# print(greetings.greet("Doreen"))
---
+++ greetings.py
@@ -0,0 +1,7 @@
+#Define the Function
+def greet(person):
+    return f"Good morning, {person}!"
+
+#Apply
+print(greet("Odinaka"))
+print(greet("Ifeanyi"))
